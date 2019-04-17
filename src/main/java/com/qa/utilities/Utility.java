1f3508aeba430e097b2c75e21576993776f1ad56
jayadev Lattupally <jayadevlattupally@jayadevs-MBP.attlocal.net>
2019-04-17 13:31:56 -0400

    q

---
+++ src/main/java/com/qa/utilities/Utility.java
@@ -0,0 +1,9 @@
+package com.qa.utilities;
+
+public class Utility {
+
+	
+	public void utility(){
+		System.out.println("This is Utility Pages");
+	}
+}
---
+++ src/test/java/com/qa/test/LoginClass.java
@@ -0,0 +1,13 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class LoginClass {
+	
+	@Test
+	
+public void test1() {
+		System.out.println("This is Test Page");
+		
+	}
+}
